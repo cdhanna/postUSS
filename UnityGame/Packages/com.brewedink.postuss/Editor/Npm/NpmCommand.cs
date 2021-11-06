75b2132bd4976f2c29e6c43fa97175e14e9beed1
Chris Hanna <cdhannaphone@gmail.com>
2021-11-06 12:16:39 -0400

    stuff

---
+++ UnityGame/Packages/com.brewedink.postuss/Editor/Npm/NpmCommand.cs
@@ -0,0 +1,203 @@
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+namespace BrewedInk.PostUSS.Npm
+{
+
+   public abstract class NpmCommand
+   {
+      const int PROCESS_NOT_FOUND_EXIT_CODE = 127; // TODO: Check this for windows?
+
+      private Process _process;
+      private TaskCompletionSource<int> _status, _standardOutComplete;
+
+      private bool _started, _hasExited;
+      protected int _exitCode = -1;
+      protected string DockerCmd => "npm";
+      public Action<int> OnExit;
+
+      public bool WriteLogToUnity { get; set; }
+      public bool WriteCommandToUnity { get; set; }
+
+
+      public abstract string GetCommandString();
+
+
+      protected virtual void HandleOnExit()
+      {
+      }
+
+      protected virtual void HandleStandardOut(string data)
+      {
+         if (_hasExited && data == null)
+         {
+            _standardOutComplete.TrySetResult(0);
+         }
+
+         if (WriteLogToUnity && data != null)
+         {
+            LogInfo(data);
+         }
+      }
+
+      protected virtual void HandleStandardErr(string data)
+      {
+         if (WriteLogToUnity && data != null)
+         {
+            LogError(data);
+         }
+      }
+
+      public virtual void Start()
+      {
+         if (_process != null)
+         {
+            throw new Exception("Process already started.");
+         }
+
+         var command = GetCommandString();
+         /*do not await. It will keep it on a separate thread, which is very important. */
+
+         Run(command);
+      }
+
+      public void Join()
+      {
+         _status.Task.Wait();
+      }
+
+      public void Kill()
+      {
+         if (_process == null || !_started || _hasExited) return;
+
+         _process.Kill();
+         try
+         {
+         }
+         catch (InvalidOperationException ex)
+         {
+            Debug.LogWarning("Unable to stop process, but likely was already stopped. " + ex.Message);
+         }
+
+      }
+
+      protected void LogInfo(string data)
+      {
+         Debug.Log(data);
+      }
+
+      protected void LogError(string data)
+      {
+         Debug.LogError(data);
+      }
+
+      protected virtual void ModifyStartInfo(ProcessStartInfo processStartInfo)
+      {
+
+      }
+
+
+      async void Run(string command)
+      {
+         try
+         {
+            if (WriteCommandToUnity)
+            {
+               Debug.Log("============== Start Executing [" + command + "] ===============");
+            }
+
+            using (_process = new System.Diagnostics.Process())
+            {
+
+#if UNITY_EDITOR && !UNITY_EDITOR_WIN
+               _process.StartInfo.FileName = "sh";
+               _process.StartInfo.Arguments = $"-c '{command}'";
+#else
+         _process.StartInfo.FileName = "cmd.exe";
+         _process.StartInfo.Arguments = $"/C {command}";  //  "/C " + command + " > " + commandoutputfile + "'"; // TODO: I haven't tested this since refactor.
+#endif
+               // Configure the process using the StartInfo properties.
+               _process.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Normal;
+               _process.EnableRaisingEvents = true;
+               _process.StartInfo.RedirectStandardInput = true;
+               _process.StartInfo.RedirectStandardOutput = true;
+               _process.StartInfo.RedirectStandardError = true;
+               _process.StartInfo.CreateNoWindow = true;
+               _process.StartInfo.UseShellExecute = false;
+               ModifyStartInfo(_process.StartInfo);
+
+
+               _status = new TaskCompletionSource<int>();
+               _standardOutComplete = new TaskCompletionSource<int>();
+               EventHandler eh = (s, e) =>
+               {
+                  // there still may pending log lines, so we need to make sure they get processed before claiming the process is complete
+                  _hasExited = true;
+                  _exitCode = _process.ExitCode;
+
+                  OnExit?.Invoke(_process.ExitCode);
+                  HandleOnExit();
+
+                  _status.TrySetResult(0);
+               };
+
+               _process.Exited += eh;
+
+
+               try
+               {
+                  _process.EnableRaisingEvents = true;
+
+                  _process.OutputDataReceived += (sender, args) => {
+                     EditorApplication.delayCall += () => {
+                        try {
+                           HandleStandardOut(args.Data);
+                        }
+                        catch (Exception ex) {
+                           Debug.LogException(ex);
+                        }
+                     };
+                  };
+                  _process.ErrorDataReceived += (sender, args) => {
+                     EditorApplication.delayCall += () => {
+                        try {
+                           HandleStandardErr(args.Data);
+                        }
+                        catch (Exception ex) {
+                           Debug.LogException(ex);
+                        }
+                     };
+                  };
+
+
+                  _process.Start();
+                  _started = true;
+                  _process.BeginOutputReadLine();
+                  _process.BeginErrorReadLine();
+
+                  await _status.Task;
+
+               }
+               finally
+               {
+                  _process.Exited -= eh;
+               }
+
+               if (WriteCommandToUnity)
+               {
+                  Debug.Log("============== End ===============");
+               }
+            }
+         }
+         catch (Exception e)
+         {
+            Debug.LogException(e);
+         }
+      }
+
+   }
+}
---
+++ UnityGame/Packages/com.brewedink.postuss/Editor/PostUssEditor.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace BrewedInk.PostUSS
+{
+    public static class PostUssEditor
+    {
+        public const string CONFIG_PATH = "Assets/PostUSS/Editor/";
+        public const string NODE_PROJECT_PATH = CONFIG_PATH + "~/";
+        public const string PACKAGE_JSON_PATH = NODE_PROJECT_PATH + "package.json";
+        public const string CONFIG_FILE_PATH = CONFIG_PATH + "postUss.asset";
+
+        [MenuItem("BrewedInk/Test")]
+        public static void Test()
+        {
+            InitProject();
+        }
+
+        public static void InitProject()
+        {
+            Directory.CreateDirectory(CONFIG_PATH);
+            Directory.CreateDirectory(NODE_PROJECT_PATH);
+
+
+            // is there a configuration file?
+            var config = AssetDatabase.LoadAssetAtPath<PostUssConfiguration>(CONFIG_FILE_PATH);
+            if (!config)
+            {
+                config = ScriptableObject.CreateInstance<PostUssConfiguration>();
+
+                config.description = "Post USS Config File";
+                config.compilePath = PACKAGE_JSON_PATH;
+                config.plugins = new List<StringPair>
+                {
+                    new StringPair("postcss", "^8.3.11"),
+                    new StringPair("postcss-cli", "^9.0.2"),
+                    new StringPair("postcss-css-variables", "^0.18.0"),
+                    new StringPair("postcss-import", "^14.0.2"),
+                    new StringPair("postcss-nested", "^5.0.6"),
+                    new StringPair("postcss-nested-props", "^2.0.0"),
+                };
+
+                AssetDatabase.CreateAsset(config, CONFIG_FILE_PATH);
+            }
+
+        }
+    }
+}
